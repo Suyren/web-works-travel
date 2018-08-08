6fe2b165953bffbbfb38642c438a44c5613e35e5
Suyren <lakers40582@gmail.com>
2018-08-08 23:21:00 +0800

    Update all.js

--- js/all.js
+++ js/all.js
@@ -11,19 +11,16 @@ var app = new Vue({
 		var vm = this;
 		axios.get('https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97')
 		.then(function (response) {
-			// console.log(response);
-			// console.log(response.data.result.records);
 			vm.ajaxData = response.data.result.records;
+			
 			var zone = response.data.result.records.map(function(arrayName){
 				return arrayName.Zone;
 			});
-			// console.log(zone);
 			zone.forEach(function(value){
 				if(vm.arrayArea.indexOf(value) == -1){
 					vm.arrayArea.push(value);
 				}
 			});
-			// console.log(vm.arrayArea);
 		})
 		.catch(function (error) {
 			console.log(error);
